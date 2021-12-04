f0ea77fcac5f781a80ea77f5247e1e64178ca590
Matthias Gieseke <matthias-gieseke@outlook.com>
2021-12-04 17:31:49 +0100

    Add project files.

---
+++ mwgWebstartShortcut/mwgWebstartShortcut.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace mwgWebstartShortcut
+{
+    partial class mwgWebstartShortcut
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mwgWebstartShortcut));
+            this.startButton = new System.Windows.Forms.Button();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(40, 68);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(176, 53);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(69, 25);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.PlaceholderText = "MWG IP address";
+            this.textBoxIP.Size = new System.Drawing.Size(118, 23);
+            this.textBoxIP.TabIndex = 1;
+            // 
+            // mwgWebstartShortcut
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 145);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.startButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mwgWebstartShortcut";
+            this.Text = "MWG Webstart Shortcut";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox textBoxIP;
+    }
+}
+
---
+++ mwgWebstartShortcut/mwgWebstartShortcut.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mwgWebstartShortcut
+{
+    public partial class mwgWebstartShortcut : Form
+    {
+        public mwgWebstartShortcut()
+        {
+            InitializeComponent();
+        }
+
+        string mwgIP;
+        string _filePath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            mwgIP = textBoxIP.Text;
+            string[] createText = { "<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "<jnlp spec=\"1.0 + \" codebase=\"http://" + mwgIP + ":4711/Konfigurator/\" href=\"webstart.jnlp\">", "	<information>", "		<title>McAfee Web Gateway</title>", "		<vendor>McAfee</vendor>", "		<icon href=\"images/icon.png\"/>", "		<icon href=\"images/icon.png\" kind=\"shortcut\" />", "		<description>Management interface of McAfee Web Gateway</description>", "		<description kind=\"short\">McAfee Web Gateway", "		</description>", "		<shortcut online=\"true\" install=\"true\">", "			<desktop />", "			<menu submenu=\"McAfee\">", "				<menu submenu=\"McAfee Web Gateway\" />", "			</menu>", "		</shortcut>", "	</information>", "	<resources>", "	 <property name=\"jnlp.packEnabled\" value=\"true\"/>", "	 <j2se version=\"1.9+\" java-vm-args=\"--add-exports=java.desktop/com.sun.java.swing.plaf.windows=ALL-UNNAMED --add-modules=java.se.ee --add-modules=java.xml.bind  --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED\"/>", "	 <j2se version=\"1.7+\" />", "	 <jar href=\"jar/webstart.jar\" download=\"eager\" main=\"true\"/>", "	</resources>", "	<application-desc main-class=\"com.scur.k.Application\"/>", "	<security>", "		<all-permissions />", "	</security>", "</jnlp>" };
+            File.WriteAllLines(Path.Combine(_filePath, "webstart.jnlp"), createText, Encoding.UTF8);
+
+            //System.Diagnostics.Process.Start(Path.Combine(_filePath, "webstart.jnlp"));
+            //strCmdText = "javaws " + Path.Combine(_filePath, "webstart.jnlp");
+            //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            //MessageBox.Show("javaws " + Path.Combine(_filePath, "webstart.jnlp"));
+
+            Process myProcess = new Process();
+            myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            myProcess.StartInfo.CreateNoWindow = true;
+            myProcess.StartInfo.UseShellExecute = false;
+            myProcess.StartInfo.FileName = "cmd.exe";
+            myProcess.StartInfo.Arguments = "/c " + ("javaws " + Path.Combine(_filePath, "webstart.jnlp"));
+            myProcess.EnableRaisingEvents = true;
+            myProcess.Start();
+            myProcess.WaitForExit();
+
+
+
+        }
+    }
+}
