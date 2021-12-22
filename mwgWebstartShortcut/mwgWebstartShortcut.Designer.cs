36f4a9d3dcf0db7b5232a3c7103bbc15e13b64e4
Matthias Gieseke <matthias-gieseke@outlook.com>
2021-12-22 20:30:21 +0100

    changed icon // adjusted labels

--- mwgWebstartShortcut/mwgWebstartShortcut.Designer.cs
+++ mwgWebstartShortcut/mwgWebstartShortcut.Designer.cs
@@ -32,11 +32,12 @@ namespace mwgWebstartShortcut
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mwgWebstartShortcut));
             this.startButton = new System.Windows.Forms.Button();
             this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(40, 68);
+            this.startButton.Location = new System.Drawing.Point(45, 80);
             this.startButton.Name = "startButton";
             this.startButton.Size = new System.Drawing.Size(176, 53);
             this.startButton.TabIndex = 2;
@@ -46,17 +47,27 @@ namespace mwgWebstartShortcut
             // 
             // textBoxIP
             // 
-            this.textBoxIP.Location = new System.Drawing.Point(69, 25);
+            this.textBoxIP.Location = new System.Drawing.Point(73, 39);
             this.textBoxIP.Name = "textBoxIP";
             this.textBoxIP.PlaceholderText = "MWG IP address";
             this.textBoxIP.Size = new System.Drawing.Size(118, 23);
             this.textBoxIP.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "MWG IP address";
             // 
             // mwgWebstartShortcut
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(262, 145);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxIP);
             this.Controls.Add(this.startButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -71,6 +82,7 @@ namespace mwgWebstartShortcut
 
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.Label label1;
     }
 }
 
